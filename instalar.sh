a2b53d0f45455789e67042c4806ae537b6ad733b
Daniel Torres <daniel.torres@owasp.org>
2018-08-05 21:28:46 -0400

    update

---
+++ instalar.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+OKBLUE='\033[94m'
+OKRED='\033[91m'
+OKGREEN='\033[92m'
+RESET='\e[0m'
+
+
+function print_ascii_art {
+cat << "EOF"
+   
+ GOOGLE
+                                                  
+
+					daniel.torres@owasp.org
+					https://github.com/DanielTorres1
+
+EOF
+}
+
+
+print_ascii_art
+
+echo -e "$OKBLUE [+] Instalando Google search $RESET" 
+cd googlesearch
+sudo cpan .
+cd ..
+
+
+sudo cp google.pl /usr/bin/google.pl
+sudo chmod a+x /usr/bin/google.pl
+
+
