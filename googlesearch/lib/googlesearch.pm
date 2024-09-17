42b107d65a3848e09241471ce043edcaae73d0c4
Daniel <daniel.torres@owasp.org>
2024-09-17 08:28:14 -0600

    update

--- googlesearch/lib/googlesearch.pm
+++ googlesearch/lib/googlesearch.pm
@@ -69,7 +69,7 @@ sub search  {
 	print "status ($status)\n";	
 	
 	if ($content =~ /Name or service not known/m || $status =~ /Too Many Requests/m) {
-	   if ($tries == 5)
+	   if ($tries == 7)
 			{die;}
 	   $tries++;
 	   goto SEARCH;		 
